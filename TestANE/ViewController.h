3f03a6d0dec66b4f24c5efe1ef3aea988de3d000
Koan-Sin Tan <koansin.tan@gmail.com>
2018-11-10 12:26:44 +0800

    test if we can use Apple Neural Engine (ANE)

--- TestANE/ViewController.h
+++ TestANE/ViewController.h
@@ -9,7 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UILabel *aneBundleLoadedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *buildVersionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *hasANELabel;
+@property (weak, nonatomic) IBOutlet UILabel *isInternalBuildLabel;
+@property (weak, nonatomic) IBOutlet UILabel *precompiledModelCheckDisabledLabel;
 
 @end
 
--- TestANE/ViewController.m
+++ TestANE/ViewController.m
@@ -8,6 +8,14 @@
 
 #import "ViewController.h"
 
+@interface _ANEDeviceInfo : NSObject
++ (id)bootArgs;
++ (id)buildVersion;
++ (bool)hasANE;
++ (bool)isInternalBuild;
++ (bool)precompiledModelChecksDisabled;
+@end
+
 @interface ViewController ()
 
 @end
@@ -17,6 +25,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSBundle *aneBundle = [NSBundle bundleWithPath:@"/System/Library/PrivateFrameworks/AppleNeuralEngine.framework"];
+    BOOL success = [aneBundle load];
+    NSLog(@"load results: %d", success);
+
+    Class _ANEDeviceInfo = NSClassFromString(@"_ANEDeviceInfo");
+
+    self.aneBundleLoadedLabel.text = [NSString stringWithFormat:@"%s", success ? "yes" : "no"];
+    self.buildVersionLabel.text = [_ANEDeviceInfo buildVersion];
+    self.hasANELabel.text = [NSString stringWithFormat:@"%s", [_ANEDeviceInfo hasANE] ? "yes" : "no"];
+    self.isInternalBuildLabel.text = [NSString stringWithFormat:@"%s", [_ANEDeviceInfo isInternalBuild] ? "yes" : "no"];
+    self.precompiledModelCheckDisabledLabel.text = [NSString stringWithFormat:@"%s", [_ANEDeviceInfo precompiledModelChecksDisabled] ? "yes" : "no"];
 }
 
 
